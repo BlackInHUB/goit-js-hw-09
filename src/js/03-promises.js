da5159a7ed5ff996f70135797cf6b5333faff20f
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-08-03 16:30:20 +0300

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const formField = document.querySelector('.form');
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+let position = 1;
+
+formField.addEventListener('submit', onSubmitBtnClick);
+
+function onSubmitBtnClick(evt) {
+  evt.preventDefault();
+  let delay = Number(delayInput.value);
+
+  for (let i = 0; i < amountInput.value; i += 1) {
+    createPromise(position, delay);
+    position += 1;
+    delay += Number(stepInput.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise(() => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
